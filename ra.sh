bc73ff0e65c02fc5c9c6ac91c4ea61bfadc0a4a9
Oszku <oszkuu@gmail.com>
2022-06-15 15:28:04 +0200

    fix

--- ra.sh
+++ ra.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-sudo pacman -Sy
-sudo pacman -S archlinux-keyring --noconfim
-sudo pacman -S wget curl fakeroot python build patch make gcc cmake pkg-config --noconfirm
 sudo rm -rf /etc/pacman.conf
 sudo mv pacman.conf /etc/pacman.conf
 sudo rm -rf /etc/pacman.d/mirrorlist
 sudo mv mirrorlist /etc/pacman.d/mirrorlist
 sudo rm -rf /etc/makepkg.conf
 sudo mv makepkg.conf /etc/makepkg.conf
+sudo pacman -Sy
+sudo pacman -S archlinux-keyring --noconfim
+sudo pacman -S wget curl fakeroot python build patch make gcc cmake pkg-config --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
