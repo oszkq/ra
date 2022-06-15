b5e367b2898618a527d874b12c643b2926dfac43
Oszku <39272319+Oszku@users.noreply.github.com>
2022-06-15 17:44:28 +0200

    Update ra.sh

--- ra.sh
+++ ra.sh
@@ -10,7 +10,7 @@ sudo pacman --noconfirm -S archlinux-keyring --noconfim
 sudo pacman --noconfirm -S wget curl fakeroot python patch make gcc cmake pkg-config 
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg --noconfirm -si
 cd ..
 rm -rf yay
 sudo pacman --noconfirm -Syyu
